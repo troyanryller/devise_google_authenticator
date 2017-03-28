588410ec4b2ea5cb9a6d0f2e23a97bd8c82d67c8
Igor Petrukh <pro100igor@gmail.com>
2017-03-28 18:22:46 +0300

    Added rails 5.1 support.

--- lib/devise_google_authenticatable/rails.rb
+++ lib/devise_google_authenticatable/rails.rb
@@ -1,6 +1,6 @@
 module DeviseGoogleAuthenticator
   class Engine < ::Rails::Engine # :nodoc:
-    ActionDispatch::Callbacks.to_prepare do
+    (DeviseGoogleAuthenticator.rails5? ? ActiveSupport::Reloader : ActionDispatch::Callbacks).to_prepare do
       DeviseGoogleAuthenticator::Patches.apply
     end
   end
